2d92dc07414c20595ed7522dffedaff2574751b6
ChrisHPZ <chrisai36@gmail.com>
2024-06-09 15:18:08 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function getYear() {
+    const d = new Date();
+    let year = d.getFullYear();
+    return "&copy" + year;
+}
+document.getElementById("footer").innerHTML = getYear();
+
+$(document).ready(function(){
+ $("nav li a").click(function(e){
+    e.preventDefault();
+    var divID = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(divID).offset().top - $("nav").outerHeight()
+    }, 750);
+ });   
+});
